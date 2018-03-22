NVIDIAGameWorks/dxvk-remix

commit e1dbab29db02e9879aec0b0b886f77b1cc1dab33

[d3d11] Implement D3D11_MAP_FLAG_DO_NOT_WAIT
Improves framerate in Dishonored 2 by up to 50%.

src/d3d11/d3d11_context_imm.cpp
@@ -347,6 +347,9 @@ namespace dxvk {
     SynchronizeCsThread();
     
     if (Resource->isInUse()) {
+      if (MapFlags & D3D11_MAP_FLAG_DO_NOT_WAIT)
+        return false;
+      
       // TODO implement properly in DxvkDevice
       while (Resource->isInUse())
         std::this_thread::yield();

commit 7c8363cd36f7a606c93329fa7468314ae22c8e33

[d3d11] Do not forward D3D11_MAP_FLAG_DO_NOT_WAIT for mapped images
Workaround for a regression in The Witcher 3 that was introduced
in commit e1dbab29db02e9879aec0b0b886f77b1cc1dab33. May have a
significant negative impact on performance in some games.

src/d3d11/d3d11_context_imm.cpp
@@ -222,7 +222,10 @@ namespace dxvk {
     if (pResource->GetMapMode() == D3D11_COMMON_TEXTURE_MAP_MODE_DIRECT) {
       const VkImageType imageType = mappedImage->info().type;
       
-      // Wait for the resource to become available
+      // Wait for the resource to become available. Forwarding
+      // DO_NOT_WAIT would break The Witcher 3 for some reason.
+      MapFlags &= ~D3D11_MAP_FLAG_DO_NOT_WAIT;
+      
       if (!WaitForResource(mappedImage, MapFlags))
         return DXGI_ERROR_WAS_STILL_DRAWING;
       