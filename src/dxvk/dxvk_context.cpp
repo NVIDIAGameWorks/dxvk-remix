NVIDIAGameWorks/dxvk-remix

commit ee43996778959b43caa5a23ee3cad0eaa85264fd

[dxvk] Avoid redundant descriptor set updates when binding buffers
Reduces unnecessary overhead when binding a different range of the
same buffer, e.g. through D3D11 VSSetConstantBuffers1.

src/dxvk/dxvk_context.cpp
@@ -158,12 +158,20 @@ namespace dxvk {
   void DxvkContext::bindResourceBuffer(
           uint32_t              slot,
     const DxvkBufferSlice&      buffer) {
-    if (!m_rc[slot].bufferSlice.matches(buffer)) {
+    if (!m_rc[slot].bufferSlice.matchesBuffer(buffer)) {
       m_rc[slot].bufferSlice = buffer;
       
       m_flags.set(
         DxvkContextFlag::CpDirtyResources,
         DxvkContextFlag::GpDirtyResources);
+    } else if (!m_rc[slot].bufferSlice.matchesRange(buffer)) {
+      m_rc[slot].bufferSlice.updateRange(
+        buffer.offset(),
+        buffer.length());
+      
+      m_flags.set(
+        DxvkContextFlag::CpDirtyDescriptorOffsets,
+        DxvkContextFlag::GpDirtyDescriptorOffsets);
     }
   }
   

src/dxvk/dxvk_buffer.h
@@ -434,7 +434,7 @@ namespace dxvk {
         ? m_buffer->mapPtr(m_offset + offset)
         : nullptr;
     }
-    
+
     /**
      * \brief Checks whether two slices are equal
      * 
@@ -447,6 +447,45 @@ namespace dxvk {
       return this->m_buffer == other.m_buffer
           && this->m_offset == other.m_offset
           && this->m_length == other.m_length;
+    }
+
+    /**
+     * \brief Checks whether two slices are from the same buffer
+     *
+     * This returns \c true if the two slices are taken
+     * from the same buffer, but may have different ranges.
+     * \param [in] other The slice to compare to
+     * \returns \c true if the buffer objects are the same
+     */
+    bool matchesBuffer(const DxvkBufferSlice& other) const {
+      return this->m_buffer == other.m_buffer;
+    }
+
+    /**
+     * \brief Checks whether two slices have the same range
+     * 
+     * This returns \c true if the two slices have the same
+     * offset and size, even if the buffers are different.
+     * May be useful if the buffers are know to be the same.
+     * \param [in] other The slice to compare to
+     * \returns \c true if the buffer objects are the same
+     */
+    bool matchesRange(const DxvkBufferSlice& other) const {
+      return this->m_offset == other.m_offset
+          && this->m_length == other.m_length;
+    }
+
+    /**
+     * \brief Changes offset and size, but not the buffer
+     *
+     * Slightly faster alternative to overriding the full
+     * buffer slice object in case only the range changes.
+     * \param [in] offset New slice offset
+     * \param [in] length New slice length
+     */
+    void updateRange(VkDeviceSize offset, VkDeviceSize length) {
+      m_offset = offset;
+      m_length = length;
     }
     
   private:

commit 03e52afdf77d1acc3d3faaae2b07ccff32eb0577

[dxvk] Avoid redundant descriptor set updates when binding buffers (v2)
We need to check not just the buffer object but also the length of the
bound buffer range, since this information will be written into the
descriptor and cannot be changed via dynamic offsets.

Fixes: ee43996 ('[dxvk] Avoid redundant descriptor set updates when binding buffers')

src/dxvk/dxvk_context.cpp
@@ -148,12 +148,24 @@ namespace dxvk {
   void DxvkContext::bindResourceBuffer(
           uint32_t              slot,
     const DxvkBufferSlice&      buffer) {
-    m_rc[slot].bufferSlice = buffer;
-    m_rcTracked.clr(slot);
+    bool needsUpdate = !m_rc[slot].bufferSlice.matchesBuffer(buffer);
 
-    m_flags.set(
-      DxvkContextFlag::CpDirtyResources,
-      DxvkContextFlag::GpDirtyResources);
+    if (likely(needsUpdate))
+      m_rcTracked.clr(slot);
+    else
+      needsUpdate = m_rc[slot].bufferSlice.length() != buffer.length();
+
+    if (likely(needsUpdate)) {
+      m_flags.set(
+        DxvkContextFlag::CpDirtyResources,
+        DxvkContextFlag::GpDirtyResources);
+    } else {
+      m_flags.set(
+        DxvkContextFlag::CpDirtyDescriptorOffsets,
+        DxvkContextFlag::GpDirtyDescriptorOffsets);
+    }
+
+    m_rc[slot].bufferSlice = buffer;
   }
   
   