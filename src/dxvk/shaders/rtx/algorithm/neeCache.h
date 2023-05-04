a145688ba6e636805db6735b3234368de5d0e18f
Yaobin Ouyang <yaobino@nvidia.com>
2023-05-04 11:54:52 -0400

    Add update shader

--- src/dxvk/shaders/rtx/algorithm/neeCache.h
+++ src/dxvk/shaders/rtx/algorithm/neeCache.h
@@ -23,6 +23,85 @@
 
 #define RADIANCE_CACHE_PROBE_RESOLUTION 20
 #define RADIANCE_CACHE_ELEMENTS 16
+// Element size in bytes
+#define RADIANCE_CACHE_ELEMENT_SIZE 4 * 2
 
 #ifndef __cplusplus
+
+struct NEECache
+{
+  static int cellToAddress(int3 cellID)
+  {
+    int idx =
+      cellID.z * RADIANCE_CACHE_PROBE_RESOLUTION * RADIANCE_CACHE_PROBE_RESOLUTION +
+      cellID.y * RADIANCE_CACHE_PROBE_RESOLUTION +
+      cellID.x;
+
+    return idx * RADIANCE_CACHE_ELEMENTS * RADIANCE_CACHE_ELEMENT_SIZE;
+  }
+
+  static int pointToAddress(vec3 position)
+  {
+    vec3 cameraPos = cameraGetWorldPosition(cb.camera);
+    float extend = 1000;
+    vec3 origin = cameraPos - extend * 0.5;
+    vec3 UVW = (position - origin) / extend;
+    if (any(UVW < 0) || any(UVW >= 1))
+    {
+      return -1;
+    }
+
+    ivec3 UVWi = UVW * RADIANCE_CACHE_PROBE_RESOLUTION;
+    return cellToAddress(UVWi);
+  }
+
+  static void clearTask(int3 cellID)
+  {
+    int address = cellToAddress(cellID);
+    RadianceCacheTask.Store(address, 0);
+  }
+
+  static int getTaskCount(int3 cellID)
+  {
+    int address = cellToAddress(cellID);
+    uint count = RadianceCacheTask.Load(address);
+    return min(count, RADIANCE_CACHE_ELEMENTS - 1);
+  }
+
+  static int getTaskAddress(int3 cellID, int taskID)
+  {
+    if (taskID >= RADIANCE_CACHE_ELEMENTS - 1)
+    {
+      return -1;
+    }
+
+    int baseAddress = cellToAddress(cellID);
+    return baseAddress + (taskID + 1) * RADIANCE_CACHE_ELEMENT_SIZE;
+  }
+
+  static int2 getTask(int3 cellID, int taskID)
+  {
+    int address = getTaskAddress(cellID, taskID);
+    if (address == -1)
+    {
+      return int2(-1);
+    }
+    return RadianceCacheTask.Load2(address);
+  }
+
+  static bool insertTask(int3 cellID, uint2 task)
+  {
+    int baseAddress = cellToAddress(cellID);
+    uint oldLength;
+    RadianceCacheTask.InterlockedAdd(baseAddress, 1, oldLength);
+
+    if (oldLength < RADIANCE_CACHE_ELEMENTS - 1)
+    {
+      RadianceCacheTask.Store2(baseAddress + (oldLength + 1) * RADIANCE_CACHE_ELEMENT_SIZE, task);
+      return true;
+    }
+    return false;
+  }
+  
+}
 #endif
