9e797924d113679894cfee52f5851d1828b9240d
Sascha Sertel <SSertel@nvidia.com>
2023-08-01 18:28:08 -0700

    Fixed an issue in get_env.py that could make the build fail

--- scripts-common/get_env.py
+++ scripts-common/get_env.py
@@ -3,9 +3,9 @@ import os
 
 if len(sys.argv) != 2:
     print("ERROR: Must provide one and only one envvar name to get_env.py", sys.stderr)
-    exit(1)
+    os._exit(0)
 envvar = sys.argv[1]
 if envvar in os.environ.keys():
     val = os.environ[envvar]
     print(val)
-exit(0)
+os._exit(0)
