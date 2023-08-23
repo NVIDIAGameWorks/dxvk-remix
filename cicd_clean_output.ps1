23168d27637c30a233b0a7f416f7d7b876c8a265
Nico <55258193+anchorlightforge@users.noreply.github.com>
2023-08-23 14:00:12 -0400

    Delete cicd_clean_output.ps1 (#15)

--- cicd_clean_output.ps1
+++
@@ -1,4 +0,0 @@
-New-Item ".\_release" -Type Directory
-Get-ChildItem -Path ".\_output" -Recurse -Filter *.pdb | Copy-Item
-Get-ChildItem -Path ".\_output" -Recurse -Filter *.dll | Copy-Item
-Get-ChildItem -Path ".\_output" -Recurse -Filter *.txt | Copy-Item
