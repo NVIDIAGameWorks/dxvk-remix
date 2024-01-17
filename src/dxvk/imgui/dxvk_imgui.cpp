595dcf1e4d6094f719fa2f5779256461c2e1322e
adunn <adunn@nvidia.com>
2024-01-16 18:00:38 -0800

    Remove legacy warning about assets not being production ready

--- src/dxvk/imgui/dxvk_imgui.cpp
+++ src/dxvk/imgui/dxvk_imgui.cpp
@@ -1270,13 +1270,6 @@ namespace dxvk {
     auto common = ctx->getCommonObjects();
     std::vector<std::string> hudMessages;
 
-    if (common->getSceneManager().areReplacementsLoaded()) {
-      const std::string& exportStatus = common->getSceneManager().getReplacementStatus();
-      if (exportStatus != "Release Ready") {
-        hudMessages.emplace_back(str::format("Warning: Replacements are not production ready. Status: ", exportStatus));
-      }
-    }
-
     if(common->getSceneManager().areReplacementsLoading())
       hudMessages.emplace_back("Loading enhancements...");
 
