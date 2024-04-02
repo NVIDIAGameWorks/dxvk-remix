6c49bdc277106a9bac0b388a2357ab9874597198
xoxor4d <45299104+xoxor4d@users.noreply.github.com>
2024-04-02 10:48:28 +0200

    use AABB to better differentiate instances
    - add `getCentroid` to `AxisAlignedBoundingBox`
    - add `getTransformedCentroid` to `AxisAlignedBoundingBox`
    - use transformed bounding-box-centroid (when AABB is valid) instead of just the world transform of the instance in `InstanceManager::findSimilarInstance`
    - ^ in `RtInstance::onTransformChanged`
    - ^ in `RtInstance::teleport`
    - ^ in `DrawCallCache::get`
    - add `AABB's For Instance Differentiation` setting to the Heuristics tab
    - add setting check to `RtxOptions::needsMeshBoundingBox`

--- src/dxvk/rtx_render/rtx_options.h
+++ src/dxvk/rtx_render/rtx_options.h
@@ -333,6 +333,8 @@ namespace dxvk {
     RTX_OPTION("rtx", uint32_t, minPrimsInStaticBLAS, 1000, "");
     RTX_OPTION("rtx", uint32_t, maxPrimsInMergedBLAS, 50000, "");
 
+    RTX_OPTION("rtx", bool, instanceUseBoundingBox, false, "Use AABB's to better differentiate instances");
+
     // Camera
     RW_RTX_OPTION_ENV("rtx", bool, shakeCamera, false, "RTX_FREE_CAMERA_ENABLE_ANIMATION", "Enables animation of the free camera.");
     RTX_OPTION_ENV("rtx", CameraAnimationMode, cameraAnimationMode, CameraAnimationMode::CameraShake_Pitch, "RTX_FREE_CAMERA_ANIMATION_MODE", "Free camera's animation mode.");
