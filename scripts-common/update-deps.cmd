1cf3f255558097559a7b4dfd9ca3f9b1bf49830f
adunn <adunn@nvidia.com>
2025-07-18 14:46:32 -0700

    Dont run packman unless a change occurs to any of the input files

--- scripts-common/update-deps.cmd
+++ scripts-common/update-deps.cmd
@@ -1,9 +1,18 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
+
+REM %1 Input file for packman (xml file)
+REM %2 Output file, in this case a log file
+
+REM resolve %1/%2 to full paths
+set "inputFile=%~f1"
+set "outputFile=%~f2"
+
 pushd %~dp0
 
-call ..\scripts-common\packman\packman pull ..\packman-external.xml
+call packman\packman pull "!inputFile:\=/!"
 if errorlevel 1 (
     echo packman error, aborting!
     exit /B 1
 )
+echo Successfully updated deps > "!outputFile!"
