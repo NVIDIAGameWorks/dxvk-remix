785e18c2764fca94e68b6e05682be7e31a26f8f3
Nuno Subtil <nsubtil@nvidia.com>
2025-08-19 16:33:45 -0700

    dlfg: Check output extents on dispatch
    In some cases the output window extents can change without the presenter
    being reinitialized. Check output extents on dispatch and reset the FG
    context if needed instead of doing it from the presenter.

--- src/dxvk/rtx_render/rtx_dlfg.cpp
+++ src/dxvk/rtx_render/rtx_dlfg.cpp
@@ -146,8 +146,6 @@ namespace dxvk {
     m_presentThread.threadHandle = dxvk::thread([this]() { runPresentThread(); });
     m_pacerThread.threadHandle = dxvk::thread([this]() { runPacerThread(); });
 
-    // note: this is the size of the window client area, which isn't necessarily the same as the D3D9 swapchain size
-    device->getCommon()->metaDLFG().setDisplaySize(uint2(desc.imageExtent.width, desc.imageExtent.height));
   }
 
   DxvkDLFGPresenter::~DxvkDLFGPresenter() {
@@ -1181,7 +1179,8 @@ namespace dxvk {
     : CommonDeviceObject(device)
     // xxxnsubtil: use swapchain frame count here
     , m_dlfgEvalCommandLists(device, 1)
-    , m_dlfgFrameEndSemaphore(RtxSemaphore::createTimeline(device, "DLFG frame end")) {
+    , m_dlfgFrameEndSemaphore(RtxSemaphore::createTimeline(device, "DLFG frame end"))
+    , m_currentDisplaySize{0, 0} {
 
     m_queryPoolDLFG = new DxvkDLFGTimestampQueryPool(m_device, kMaxFramesInFlight);
 
@@ -1227,6 +1226,15 @@ namespace dxvk {
 
     if (!m_dlfgContext) {
       m_dlfgContext = m_device->getCommon()->metaNGXContext().createDLFGContext();
+      m_contextDirty = true;
+    }
+
+    // check if the output extents have changed
+    VkExtent3D outputExtent = outputImage->imageInfo().extent;
+    if (outputExtent.width != m_currentDisplaySize[0] ||
+        outputExtent.height != m_currentDisplaySize[1]) {
+      // note: this is the size of the window client area, which isn't necessarily the same as the D3D9 swapchain size
+      setDisplaySize(uint2(outputExtent.width, outputExtent.height));
       m_contextDirty = true;
     }
 
